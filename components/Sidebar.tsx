6653d9502fed2c8ce13519819bed410a8fecf295
MarinaYano <myano2621@gmail.com>
2024-06-28 13:55:42 -0700

    update: change active route

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,11 +8,12 @@ import { usePathname } from 'next/navigation'
 
 const Sidebar = () => {
   const pathname = usePathname()
+  
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
       <div className='flex flex-1 flex-col gap-6'>
         {sidebarLinks.map((item) => {
-          const isActive = pathname === item.route || pathname.startsWith(item.route);
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}`);
 
           return (
             <Link
