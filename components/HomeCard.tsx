d4bb8bec98dd797c6ac45442b46e3727672594ee
MarinaYano <myano2621@gmail.com>
2024-06-28 14:35:35 -0700

    add: create reusable card component on home

---
+++ components/HomeCard.tsx
@@ -0,0 +1,37 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+type HomeCardProps = {
+  className?: string;
+  img: string;
+  title: string;
+  des: string;
+  handleClick?: () => void;
+
+}
+
+const HomeCard = ({ className, img, title, des, handleClick }: HomeCardProps) => {
+  return (
+    <section
+      className={cn(
+        'bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer', className
+      )}
+    >
+      <div className="flex-center glassmorphism size-12 rounded-[10px]">
+        <Image
+          src={img}
+          alt={title}
+          width={200}
+          height={200}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-lg font-normal">{des}</p>
+      </div>
+    </section>
+  )
+}
+
+export default HomeCard
